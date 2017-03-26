0bd099358507f61695f44e9f751e06ba4b432656
Paulo <paulo@pauloalencar.com>
2017-03-26 14:55:31 -0300

    Exemplo de class Pessoa

---
+++ pessoa.php
@@ -0,0 +1,17 @@
+<?php
+
+class Pessoa 
+{
+    public $nome;
+    public $idade;         
+}
+
+$pessoa1 = new Pessoa();
+$pessoa1->nome = "Paulo";
+$pessoa1->idade = 35;
+
+$pessoa2 = new Pessoa();
+$pessoa2->nome = "João";
+$pessoa2->idade = 6;
+
+echo $pessoa1->nome;
