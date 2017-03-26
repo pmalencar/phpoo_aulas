d3a9aa35f2986b814cfc4bb046fca05cfbadc029
Paulo <paulo@pauloalencar.com>
2017-03-26 18:22:47 -0300

    nova classe

---
+++ produto.php
@@ -0,0 +1,14 @@
+<?php
+
+class Produto
+{
+    public $nome;
+    public $descricao;
+    public $valor;
+    public $estoque;
+    
+    public function __construct($nome, $descricao, $valor, $estoque)
+    {
+        
+    }
+}
